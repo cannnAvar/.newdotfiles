4a1bbae8d51cfab86c329b9397794445f9956c62
cannnAvar <72388136+cannnAvar@users.noreply.github.com>
2025-11-01 18:49:33 +0300

    I don't remember what I change But I am prity shure its important

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,12 +1,27 @@
 local options = {
 	backup = false,
+	swapfile = false,
 	number = true,
 	clipboard = "unnamedplus",
 	ignorecase = true,
 	showtabline = 2,
-	pumheight = 10
+	pumheight = 10,
+	list = true,
+	listchars =
+	{
+		tab = "▏ ",
+		trail = "·",
+		extends = "»",
+		precedes = "«",
+	},
+cursorline = true,
 }
+
 
 for k, v in pairs(options) do
     vim.opt[k] = v
 end
+
+vim.g.netrw_banner = 0
+
+
